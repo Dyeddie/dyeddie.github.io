82dadf4a8cb174929dd671577b23d7beb0744745
Dyeddie <3488368271@qq.com>
2024-06-19 14:23:49 +0800

    Site updated: 2024-06-19 14:23:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["20240401/","20240402/","20240601/","20240602/","20240603/","20240604/"];function toRandomPost(){
+var posts=["20240401/","20240402/","20240601/","20240603/","20240604/","20240602/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
